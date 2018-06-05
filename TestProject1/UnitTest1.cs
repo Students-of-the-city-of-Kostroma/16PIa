7e62b1f349ce503b161de5e8834bc3349e0bfd21
Syntrx <38738439+Syntrx@users.noreply.github.com>
2018-06-05 23:35:37 +0300

    Add files via upload

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -29,7 +29,172 @@ namespace TestProject1
                 "(operator)", "+",
                 "(literal)", "3"
                 );
+        }
+       
+        [TestMethod]
+        public void TestMethod2()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("5"),
+                "(literal)", "5"
+                );
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            MathLexer l = new MathLexer();
+            CheckTokens(l.Tokenize("-3"),
+                "(operator)", "-",
+                "(literal)", "3"
+                );
+        }
 
+        [TestMethod]
+        public void TestMethod4()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-2-(-3)"),
+                "(operator)", "-",
+                "(literal)", "2",
+                "(operator)", "-",
+                "(parenthesis)", "(",
+                "(operator)", "-",
+                "(literal)", "3",
+                "(parenthesis)", ")"
+                );
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-2+(-3)"),
+                "(operator)", "-",
+                "(literal)", "2",
+                "(operator)", "+",
+                "(parenthesis)", "(",
+                "(operator)", "-",
+                "(literal)", "3",
+                "(parenthesis)", ")"
+                );
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-1"),
+                "(operator)", "-",
+                "(literal)", "1"
+                );
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("1.2"),
+                "(literal)", "1.2"
+                );
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-1.2"),
+                "(operator)", "-",
+                "(literal)", "1.2"
+                );
+        }
+        public void TestMethod9()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("90*(-4)"),
+                "(literal)", "90",
+                "(operator)", "*",
+                "(parenthesis)", "(",
+                "(operator)", "-",
+                "(literal)", "4",
+                "(parenthesis)", ")"
+                );
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-09/03"),
+                "(operator)", "-",
+                "(literal)", "09",
+                "(operator)", "/",
+                "(literal)", "03"
+                );
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-09/(-03)"),
+                "(operator)", "-",
+                "(literal)", "09",
+                "(operator)", "/",
+                "(parenthesis)", "(",
+                "(operator)", "-",
+                "(literal)", "03",
+                "(parenthesis)", ")"
+                );
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("10-7/(2)"),
+                "(literal)", "10",
+                "(operator)", "-",
+                "(literal)", "7",
+                "(operator)", "/",
+                "(parenthesis)", "(",
+                "(literal)", "2",
+                "(parenthesis)", ")"
+                );
+        }
+        [TestMethod]
+        public void TestMethod13()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("1.2+6-(-4)"),
+                "(literal)", "1.2",
+                "(operator)", "+",
+                "(literal)", "6",
+                "(operator)", "-",
+                "(parenthesis)", "(",
+                "(operator)", "-",
+                "(literal)", "4",
+                "(parenthesis)", ")"
+                );
+        }
+        [TestMethod]
+        public void TestMethod14()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-2e"),
+                "(operator)", "-",
+                "(literal)", "2e"
+                );
+        }
+        [TestMethod]
+        public void TestMethod15()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("1.8e+6"),
+                "(literal)", "1.8e+6"
+                );
+        }
+        [TestMethod]
+        public void TestMethod16()
+        {
+            MathLexer lexer = new MathLexer();
+            CheckTokens(lexer.Tokenize("-1.8e+6"),
+                "(operator)", "-",
+                "(literal)", "1.8e+6"
+                );
         }
     }
+
 }
